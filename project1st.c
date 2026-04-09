6f440311f910c8c2885f64bc79b793e50433b73f
JashanpreetKaur-0 <joshanjashan854@gmail.com>
2026-04-09 10:36:20 +0530

    Add files via upload

---
+++ project1st.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0));
+    int randomNumber=(rand() % 100)+1;
+    int no_of_guesses=0;
+    int guessed;
+    do{
+        printf("enter the number:");
+        scanf("%d", &guessed);
+        if(guessed>randomNumber){
+            printf("lower number please\n");
+            
+        }else if (guessed<randomNumber){
+            printf("higher number please\n");
+            
+        }
+        else{
+            printf("you guessed it right!\n");
+        }
+        no_of_guesses++;
+    }while (guessed!=randomNumber);
+    printf("you guessed the number in %d guesses", no_of_guesses);
+    return 0;
+}
